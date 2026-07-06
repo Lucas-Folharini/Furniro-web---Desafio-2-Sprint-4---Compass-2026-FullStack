0cd747794e1fe5d484f75d5c4111f9e6b602bbc9
Lucas-Folharini <folharinilucas@mx2.unic.br>
2026-07-06 10:01:25 -0300

    feat: create footer component and added it to page

--- furniro-page/src/App.tsx
+++ furniro-page/src/App.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 
 function App() {
@@ -6,6 +7,8 @@ function App() {
       <Header />
 
       <main></main>
+
+      <Footer/>
     </div>
   );
 }
