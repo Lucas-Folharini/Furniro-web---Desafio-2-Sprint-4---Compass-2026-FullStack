7efb6528bcf0ffc70ad72d73fa856fc3627930b9
Lucas-Folharini <folharinilucas@mx2.unic.br>
2026-07-06 12:43:04 -0300

    feat: Creates a hero section with a background image, and added it to page

--- furniro-page/src/App.tsx
+++ furniro-page/src/App.tsx
@@ -1,12 +1,16 @@
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
+import { Hero } from "./sections/Hero";
 
 function App() {
   return (
     <div className="w-full min-h-screen bg-white antialiased selection:bg-[#B88E2F] selection:text-white">
       <Header />
 
-      <main></main>
+      <main>
+        <Hero/>
+
+      </main>
 
       <Footer/>
     </div>
