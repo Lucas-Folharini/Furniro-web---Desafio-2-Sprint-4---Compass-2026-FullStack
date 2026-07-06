548faf5b72689a5741364b97a0635a4f6b9bdda9
Lucas-Folharini <folharinilucas@mx2.unic.br>
2026-07-06 14:45:48 -0300

    feat: Creates a raw section OurProducts without jsonserver e added it to the page

--- furniro-page/src/App.tsx
+++ furniro-page/src/App.tsx
@@ -2,6 +2,7 @@ import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { BrowseRange } from "./sections/BrowseRange";
 import { Hero } from "./sections/Hero";
+import { OurProducts } from "./sections/OurProducts";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <main>
         <Hero/>
         <BrowseRange/>
+        <OurProducts/>
       </main>
 
       <Footer/>
