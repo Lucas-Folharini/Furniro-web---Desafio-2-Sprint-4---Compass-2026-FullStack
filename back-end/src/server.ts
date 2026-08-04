1088d3c29299b42c7818134789836dba29794650
pedro-galdino <pedro.galdino34@gmail.com>
2026-08-04 18:48:54 -0300

    feat: add backend docs with swagger

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -5,12 +5,15 @@ import { AppDataSource } from './database/data-source';
 import { productsRoutes } from './routes/products.routes';
 import { seedProducts } from './database/seed';
 import { errorHandler } from './shared/middlewares/error-handler';
+import swaggerUi from 'swagger-ui-express';
+import { swaggerDocument } from './docs/swagger';
 
 const app = express();
 const port = Number(process.env.PORT) || 3000;
 
 app.use(express.json());
 
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/images', express.static(path.resolve(__dirname, '../public/images')));
 app.use('/products', productsRoutes);
 
